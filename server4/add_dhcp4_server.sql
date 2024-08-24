d063e8e1c4a2b6f93e2e45d2a16fa500778325c8
Sridhar Ayengar <this.is.ayengar@gmail.com>
2024-08-24 03:15:00 -0400

    Procedures don't need specific transaction lingo.  It's implied.

--- server4/add_dhcp4_server.sql
+++ server4/add_dhcp4_server.sql
@@ -4,10 +4,8 @@ LANGUAGE plpgsql
 AS $$
 DECLARE rightnow timestamp;
 BEGIN;
-  BEGIN TRANSACTION;
   SELECT NOW() INTO rightnow;
   CALL createauditrevisiondhcp4(rightnow, tag, 'Adding server ' || tag, 0);
   INSERT INTO dhcp4_server(tag, description, modification_ts)
                     VALUES(tag, description, rightnow);
-  COMMIT;
 END $$;
