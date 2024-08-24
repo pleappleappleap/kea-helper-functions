914a8acd333051981bfcb9e52aba97aee4c55806
Sridhar Ayengar <this.is.ayengar@gmail.com>
2024-08-24 03:22:55 -0400

    Trying to fix procedure call syntax.

--- server4/add_dhcp4_server.sql
+++ server4/add_dhcp4_server.sql
@@ -2,10 +2,10 @@ CREATE OR REPLACE PROCEDURE add_dhcp4_server(tag varchar(64),
                                              description text)
 LANGUAGE plpgsql
 AS $$
-DECLARE rightnow timestamp;
+DECLARE rightnow timestamp with time zone;
 BEGIN
   SELECT NOW() INTO rightnow;
-  CALL createauditrevisiondhcp4(rightnow, tag, 'Adding server ' || tag, 0);
+  CALL createauditrevisiondhcp4(rightnow, tag, 'Adding server ' || tag, false);
   INSERT INTO dhcp4_server(tag, description, modification_ts)
                     VALUES(tag, description, rightnow);
 END $$;
